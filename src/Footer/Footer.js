42c699c8327ebe66d629273980e6147d9683f42e
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-05-22 22:18:58 +0300

    Add files via upload

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {FooterContainer,FooterSubscriptions,FooterSubHeading,FooterSubText} from './Footer.element';
+import {Button} from '../gloabalStyles'
+import {FooterContainer,FooterSubscriptions,FooterSubHeading,FooterSubText,Form,FormInput} from './Footer.element';
 function Footer() {
     return (
         <FooterContainer>
@@ -10,7 +11,47 @@ function Footer() {
                 <FooterSubText>
                     You can unscbscribe at any time 
                 </FooterSubText>
+                <Form>
+                    <FormInput name="email" type="email" placeholder="your email"/>
+                        <Button fontBig>Subscribe</Button>
+                </Form>
             </FooterSubscriptions>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItem>
+                        <FooterLinkTitle>About US</FooterLinkTitle>
+                        <FooterLink to="signup">How it works</FooterLink>
+                        <FooterLink to="signup">Testimonials</FooterLink>
+                        <FooterLink to="signup">Carrers</FooterLink>
+                        <FooterLink to="signup">Investors</FooterLink>
+                        <FooterLink to="signup">Terms of service</FooterLink>
+                    </FooterLinksItem>
+                    <FooterLinksItem>
+                        <FooterLinkTitle>Contact US</FooterLinkTitle>
+                        <FooterLink to="signup">How it works</FooterLink>
+                        <FooterLink to="signup">Testimonials</FooterLink>
+                        <FooterLink to="signup">Carrers</FooterLink>
+                        <FooterLink to="signup">Investors</FooterLink>
+                        <FooterLink to="signup">Terms of service</FooterLink>
+                    </FooterLinksItem>
+                    <FooterLinksItem>
+                        <FooterLinkTitle>Video</FooterLinkTitle>
+                        <FooterLink to="signup">How it works</FooterLink>
+                        <FooterLink to="signup">Testimonials</FooterLink>
+                        <FooterLink to="signup">Carrers</FooterLink>
+                        <FooterLink to="signup">Investors</FooterLink>
+                        <FooterLink to="signup">Terms of service</FooterLink>
+                    </FooterLinksItem>
+                    <FooterLinksItem>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to="signup">How it works</FooterLink>
+                        <FooterLink to="signup">Testimonials</FooterLink>
+                        <FooterLink to="signup">Carrers</FooterLink>
+                        <FooterLink to="signup">Investors</FooterLink>
+                        <FooterLink to="signup">Terms of service</FooterLink>
+                    </FooterLinksItem>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
         </FooterContainer>
     )
 }
